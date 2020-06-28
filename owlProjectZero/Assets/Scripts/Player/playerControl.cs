fa55cce4732a689d20f38fa87b8e94823bd60db6
Charles Advincula <chadvincula7636428@gmail.com>
2020-06-27 17:53:19 -0700

    Moved playercontrols script to a player script folder
    - Limited player jumps to 2
    - Jumps get refreshed when landing on top of a collider

--- owlProjectZero/Assets/Scripts/Player/playerControl.cs
+++ owlProjectZero/Assets/Scripts/Player/playerControl.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class playerControl : MonoBehaviour
 {
     Rigidbody rb;
+    SphereCollider sphereCollider;
+
+    private int numJumps = 2;
     
     public float groundSpeed;
     public float airSpeed;
@@ -17,16 +20,20 @@ public class playerControl : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        sphereCollider = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && numJumps > 0)
         {
             rb.AddForce(new Vector3(0, jumpDistance, 0), ForceMode.Impulse);
+            numJumps--;
         }
+
+        // Debug.DrawRay(rb.position, Vector2.down * (sphereCollider.bounds.extents.y), Color.green, 0.1f);
     }
 
     private void FixedUpdate()
@@ -38,5 +45,20 @@ public class playerControl : MonoBehaviour
     private void moveCharacter(Vector3 direction)
     {
         rb.MovePosition(transform.position + (direction * groundSpeed * Time.deltaTime));
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        foreach(ContactPoint contact in col.contacts)
+        {
+            if(contact.point.y > rb.position.y - sphereCollider.bounds.extents.y + 0.1f)
+                return;
+            else
+            {
+                numJumps = 2;
+            }
+            // Debug.DrawRay(contact.point, Vector2.up, Color.cyan, 10f);
+        }
+        // Debug.Log("Collided with " + col.gameObject.name);
     }
 }
