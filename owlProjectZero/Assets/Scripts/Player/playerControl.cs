ff120ee3e9a5b641477168b2bea2f1f4bb0bf50b
Charles Advincula <chadvincula7636428@gmail.com>
2020-06-28 20:01:23 -0700

    Changed movement mechanics
    - No more z axis movement
    - Separated ground and air speeds
    - Now directly manipulates velocity instead of moving to new position

--- owlProjectZero/Assets/Scripts/Player/playerControl.cs
+++ owlProjectZero/Assets/Scripts/Player/playerControl.cs
@@ -7,11 +7,14 @@ public class playerControl : MonoBehaviour
     Rigidbody rb;
     SphereCollider sphereCollider;
 
-    private Vector3 movement;
+    private float maxSpeed;
+    private float acceleration;
+
     public const int MAX_JUMPS = 3;
     public int numJumps;
     public float groundSpeed;
     public float airSpeed;
+    public float xSpeed;
     public float jumpDistance;
     public float dragAmount;
 
@@ -21,35 +24,81 @@ public class playerControl : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         sphereCollider = GetComponent<SphereCollider>();
+        xSpeed = rb.velocity.x;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        acceleration = Input.GetAxis("Horizontal");
         if (Input.GetButtonDown("Jump") && numJumps > 0)
         {
             // rb.AddForce(new Vector3(0, jumpDistance, 0), ForceMode.Impulse);
-            jump();
+            Jump();
             numJumps--;
         }
-
         // Debug.DrawRay(rb.position, Vector2.down * (sphereCollider.bounds.extents.y), Color.green, 0.1f);
     }
 
     private void FixedUpdate()
     {
-        moveCharacter(movement);
+        xSpeed = rb.velocity.x;
+        moveCharacter(acceleration);
+    }
+
+    // Moves the player left/right based off of the value of its acceleration
+    private void moveCharacter(float direction)
+    {
+        // if(direction != 0f || rb.velocity.magnitude < maxSpeed)
+        // {
+        //     xSpeed += direction;
+        //     Vector3 newVelocity = rb.velocity;
+        //     newVelocity[0] = xSpeed * Time.deltaTime;
+        //     rb.velocity = newVelocity;
+        // }
+
+        Vector3 newVelocity = rb.velocity;
+        newVelocity[0] = direction * maxSpeed;
+        rb.velocity = newVelocity;
         
+        // Vector3 newPos = transform.position + new Vector3(direction * xSpeed * Time.deltaTime, 0, 0);
+        // rb.MovePosition(newPos);
     }
 
-    private void moveCharacter(Vector3 direction)
+    private void Jump()
     {
-        rb.MovePosition(transform.position + (direction * groundSpeed * Time.deltaTime));
+        Vector3 newVelocity = rb.velocity;
+        newVelocity[1] = jumpDistance;
+        rb.velocity = newVelocity;
     }
 
-    private void jump()
+    private void Attack()
     {
-        rb.velocity = Vector3.up * jumpDistance;
+        // Yet to be implemented
+    }
+
+    private void Dodge()
+    {
+        // Yet to be implemented
+    }
+
+    private void ChangeSkill()
+    {
+        // Yet to be implemented
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.GetComponent<EnvironmentElement>() != null)
+        {
+            maxSpeed = groundSpeed;
+        }
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        if(col.gameObject.GetComponent<EnvironmentElement>() != null)
+        {
+            maxSpeed = airSpeed;
+        }
     }
 }
