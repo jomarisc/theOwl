85dc656e312c5affda78ce98affc6d032303ccb9
Charles Advincula <chadvincula7636428@gmail.com>
2020-06-30 23:25:08 -0700

    Created collectable prefab. Idle movement works on ground level only

--- owlProjectZero/Assets/Scripts/Miscellaneous/Collectable.cs
+++ owlProjectZero/Assets/Scripts/Miscellaneous/Collectable.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    private float heightOffset;
+    private static int numCollectables = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        heightOffset = transform.position.y;
+        Debug.Log(heightOffset);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Float(0.5f);
+        if(!gameObject.activeSelf && Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    protected void Float(float height)
+    {
+        Vector3 newPos = transform.position;
+        newPos[1] = (Mathf.Sin(Time.time) + heightOffset * 4) * (heightOffset / 4);
+        transform.position = newPos;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.GetComponent<playerControl>() != null)
+        {
+            numCollectables++;
+            Debug.Log("Collected " + numCollectables + " collectables");
+            gameObject.SetActive(false);
+        }
     }
 }
