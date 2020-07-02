e45eb252e77b8a2dd29cff4dee51a017ffd87ab3
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-02 03:30:51 -0700

    PlayerControl script inherits from Character abstract class

--- owlProjectZero/Assets/Scripts/Player/playerControl.cs
+++ owlProjectZero/Assets/Scripts/Player/playerControl.cs
@@ -2,29 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerControl : MonoBehaviour
+[RequireComponent(typeof(Collider))]
+public class playerControl : Character
 {
-    Rigidbody rb;
     SphereCollider sphereCollider;
 
-    private float maxSpeed;
     private float acceleration;
-
+    private float xSpeed;
     public const int MAX_JUMPS = 3;
-    public int numJumps;
-    public float groundSpeed;
-    public float airSpeed;
-    public float xSpeed;
-    public float jumpDistance;
-    public float dragAmount;
-
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         sphereCollider = GetComponent<SphereCollider>();
-        xSpeed = rb.velocity.x;
     }
 
     // Update is called once per frame
@@ -44,44 +35,4 @@ public class playerControl : MonoBehaviour
         MoveCharacter(acceleration);
     }
 
-    // Moves the player left/right based off of the value of its acceleration
-    private void MoveCharacter(float direction)
-    {
-        Vector3 newVelocity = rb.velocity;
-        newVelocity[0] = direction * maxSpeed;
-        rb.velocity = newVelocity;
-    }
-
-    private void Jump()
-    {
-        Vector3 newVelocity = rb.velocity;
-        newVelocity[1] = jumpDistance;
-        rb.velocity = newVelocity;
-    }
-
-    private void Attack()
-    {
-        // Yet to be implemented
-    }
-
-    private void Dodge()
-    {
-        // Yet to be implemented
-    }
-
-    private void ChangeSkill()
-    {
-        // Yet to be implemented
-    }
-
-    private void OnCollisionEnter(Collision col)
-    {
-        if(col.gameObject.GetComponent<EnvironmentElement>() != null)
-            maxSpeed = groundSpeed;
-    }
-    private void OnCollisionExit(Collision col)
-    {
-        if(col.gameObject.GetComponent<EnvironmentElement>() != null)
-            maxSpeed = airSpeed;
-    }
 }
