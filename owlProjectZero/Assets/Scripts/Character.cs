7ab0e691f53058b609634f545195abd03d78ad32
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-06 20:36:20 -0700

    Implemented attack method to character class
    - Added facing direction field as well
    - Jump method checks if numJumps > 0 on its own

--- owlProjectZero/Assets/Scripts/Character.cs
+++ owlProjectZero/Assets/Scripts/Character.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public abstract class Character : MonoBehaviour, ICharacter
 {
     // private attributes
+    [SerializeField]
+    private GameObject meleeAttack;
     private float maxSpeed;
 
     // protected attributes
@@ -16,10 +18,24 @@ public abstract class Character : MonoBehaviour, ICharacter
     public float groundSpeed;
     public float airSpeed;
     public float jumpDistance;
+    public bool isFacingRight; // Determines whether the character is
+                               //facing in the right-hand direction
 
     // Moves the player left/right based off of the value of its acceleration
     public void MoveCharacter(float direction)
     {
+        if(Mathf.Abs(direction) > 0)
+        {
+            if(direction < 0)
+            {
+                isFacingRight = false;
+            }
+            else
+            {
+                isFacingRight = true;
+            }
+        }
+        
         Vector3 newVelocity = rb.velocity;
         newVelocity[0] = direction * maxSpeed;
         rb.velocity = newVelocity;
@@ -27,14 +43,30 @@ public abstract class Character : MonoBehaviour, ICharacter
 
     public void Jump()
     {
-        Vector3 newVelocity = rb.velocity;
-        newVelocity[1] = jumpDistance;
-        rb.velocity = newVelocity;
+        if(numJumps > 0)
+        {
+            Vector3 newVelocity = rb.velocity;
+            newVelocity[1] = jumpDistance;
+            rb.velocity = newVelocity;
+            numJumps--;
+        }
     }
 
     public void Attack()
     {
         // Yet to be implemented
+        meleeAttack.gameObject.SetActive(true);
+
+        Vector3 atkPos = meleeAttack.transform.localPosition;
+        if(isFacingRight)
+        {
+            atkPos[0] = 1.5f;
+        }
+        else
+        {
+            atkPos[0] = -1.5f;
+        }
+        meleeAttack.transform.localPosition = atkPos;
     }
 
     public void Dodge()
