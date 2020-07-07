e490944caaaa254da8c18c9fea826dacece271e0
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-06 20:34:36 -0700

    Created Melee Attack class

---
+++ owlProjectZero/Assets/Scripts/MeleeAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+    public float activeFrames = 10f;
+    public float knockback = 5f;
+    public int damage = 1;
+
+    void OnEnable()
+    {
+        activeFrames = 10f;
+    }
+
+    void Update()
+    {
+        activeFrames--;
+        if(activeFrames < 0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    
+    void OnTriggerEnter(Collider col)
+    {
+        col.gameObject.SetActive(false);
+    }
+}
