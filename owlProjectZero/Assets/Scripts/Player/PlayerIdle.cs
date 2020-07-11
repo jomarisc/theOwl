f113c190dfb7aa02975d1f31cb69b20d00e59266
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-11 03:07:45 -0700

    Created PlayerIdle state
    - When in this state, the player stays in-place and is constantly checking for input to switch states
    - With no input for too long, it will play the idle animation (to be impelemented)
    - Other states have yet to be implemented, which means that the game won't compile yet

---
+++ owlProjectZero/Assets/Scripts/Player/PlayerIdle.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Player Idle State
+public class PlayerIdle : IState
+{
+    private readonly playerControl player;
+    private float waitTime = 30f; // Time until the "no input" animation kicks in 
+
+    PlayerIdle(playerControl p)
+    {
+        player = p;
+    }
+    public void Enter()
+    {
+        // Enter idle animation code here:
+    }
+
+    public void Exit()
+    {
+        // Nothing so far
+    }
+
+    public void FixedUpdate()
+    {
+        // Constantly stay in place
+        player.MoveCharacter(0f);
+    }
+
+    public IState Update()
+    {
+        
+        // Check input for horizontal movement
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            return new PlayerWalk(player);
+        }
+
+        // Check input for changing skills
+
+
+
+
+        // Check input for dodging
+        if(Input.GetButtonDown("Fire3"))
+        {
+            return new PlayerDodge(player);
+        }
+
+        // Check input for jumping
+        if(Input.GetButtonDown("Jump"))
+        {
+            player.numJumps--;
+            return new PlayerJump(player);
+        }
+
+        // Check input for melee attacking
+        if(Input.GetButtonDown("Fire1"))
+        {
+            // meleeAttack.gameObject.SetActive(true);
+            return new PlayerMelee(player);
+        }
+
+        // Check input for shooting with a projectile
+        if(Input.GetButtonDown("Fire2"))
+        {
+            return new PlayerShoot(player);
+        }
+
+        // Check idle for a while
+        if(waitTime >= 0)
+        {
+            waitTime -= Time.deltaTime;
+        }
+        else
+        {
+            // Begin "no input" animation
+        }
+        return null;
+    }
+}
