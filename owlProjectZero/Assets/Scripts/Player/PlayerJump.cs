b855107bc7bd3fecbb1172e2e5dac550535987d7
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-11 03:18:17 -0700

    Created PlayerJump state that does nothing atm

---
+++ owlProjectZero/Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : IState
+{
+    private readonly playerControl player;
+
+    public PlayerJump(playerControl p)
+    {
+        player = p;
+    }
+    public void Enter()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+
+    }
+
+    public IState Update()
+    {
+        return null;
+    }
+}
