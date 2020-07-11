a14dda74df46a357d9fa44c6ea80dd2bcb600ecb
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-11 04:13:35 -0700

    Implemented PlayerWalk state

--- owlProjectZero/Assets/Scripts/Player/PlayerWalk.cs
+++ owlProjectZero/Assets/Scripts/Player/PlayerWalk.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerWalk : IState
 {
     private readonly playerControl player;
+    private float horizontalMovement = 0f;
 
     public PlayerWalk(playerControl p)
     {
@@ -12,21 +13,56 @@ public class PlayerWalk : IState
     }
     public void Enter()
     {
-
+        // use walking animation here:
     }
 
     public void Exit()
     {
-
+        // Nothing so far
     }
 
     public void FixedUpdate()
     {
-
+        player.MoveCharacter(horizontalMovement);
     }
 
     public IState Update()
     {
+        // Check input for changing skills
+
+
+
+
+        // Check input for dodging
+        if(Input.GetButtonDown("Fire3"))
+        {
+            return new PlayerDodge(player);
+        }
+
+        // Check input for jumping
+        if(Input.GetButtonDown("Jump"))
+        {
+            player.numJumps--;
+            return new PlayerJump(player);
+        }
+
+        // Check input for melee attacking
+        if(Input.GetButtonDown("Fire1"))
+        {
+            // meleeAttack.gameObject.SetActive(true);
+            return new PlayerMelee(player);
+        }
+
+        // Check input for shooting with a projectile
+        if(Input.GetButtonDown("Fire2"))
+        {
+            return new PlayerShoot(player);
+        }
+        horizontalMovement = Input.GetAxis("Horizontal");
+        if(Mathf.Abs(horizontalMovement) > 0)
+        {
+            player.isFacingRight = (horizontalMovement < 0) ? false : true;
+        }
         return null;
     }
 }
