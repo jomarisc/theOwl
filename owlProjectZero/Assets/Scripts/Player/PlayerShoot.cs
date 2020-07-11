f3ad0d7db59e1ed927ab6472a8882965c59df99c
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-11 03:20:52 -0700

    Created PlayerShoot state that does nothing atm
    - No more compiler errors now!

---
+++ owlProjectZero/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : IState
+{
+    private readonly playerControl player;
+
+    public PlayerShoot(playerControl p)
+    {
+        player = p;
+    }
+    public void Enter()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+
+    }
+
+    public IState Update()
+    {
+        return null;
+    }
+}
