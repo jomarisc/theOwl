7f24ab80bd033dfb67d819ce4cb25c7147b6fe5a
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-11 03:19:31 -0700

    Created PlayerMelee state that does nothing atm

---
+++ owlProjectZero/Assets/Scripts/Player/PlayerMelee.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMelee : IState
+{
+    private readonly playerControl player;
+
+    public PlayerMelee(playerControl p)
+    {
+        player = p;
+    }
+    public void Enter()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+
+    }
+
+    public IState Update()
+    {
+        return null;
+    }
+}
