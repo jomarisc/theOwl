5353ddb257498b51f2090a0f2911a2cf923b8715
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-11 03:05:24 -0700

    Created State interface

---
+++ owlProjectZero/Assets/Scripts/IState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IState
+{
+    // Gets called when this becomes the current state
+    void Enter();
+    // Gets called when you leave this state
+    void Exit();
+    // Gets called for physics checking
+    void FixedUpdate();
+    // Gets called every frame if this is the current state
+    // Return null to remain in the same state
+    IState Update();
+}
