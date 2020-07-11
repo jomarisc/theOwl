7914f4d92650f598d6c35e0de4f4efc84c972762
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-11 03:17:11 -0700

    Created PlayerDodge state that does nothing atm

---
+++ owlProjectZero/Assets/Scripts/Player/PlayerDodge.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDodge : IState
+{
+    private readonly playerControl player;
+
+    public PlayerDodge(playerControl p)
+    {
+        player = p;
+    }
+    public void Enter()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+
+    }
+
+    public IState Update()
+    {
+        return null;
+    }
+}
