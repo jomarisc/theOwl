4c50fa55cf63e1712672d8b9a1414e1da4da14bf
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-12 03:27:31 -0700

    Implmented PlayerJump state

--- owlProjectZero/Assets/Scripts/Player/PlayerJump.cs
+++ owlProjectZero/Assets/Scripts/Player/PlayerJump.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerJump : IState
 {
     private readonly playerControl player;
+    private Rigidbody playerBody;
+    private float horizontalMovement = 0f;
+    // private float jumpCooldown = 0.06f;
 
     public PlayerJump(playerControl p)
     {
         player = p;
+        playerBody = p.gameObject.GetComponent<Rigidbody>();
     }
     public void Enter()
     {
+        // use jump animation here
 
+        player.Jump();
     }
 
     public void Exit()
@@ -22,11 +28,54 @@ public class PlayerJump : IState
 
     public void FixedUpdate()
     {
-
+        player.MoveCharacter(horizontalMovement);
     }
 
     public IState Update()
     {
+        // Check input for double jump
+        if(Input.GetButtonDown("Jump"))
+        {
+            // player.numJumps--;
+            return new PlayerJump(player);
+        }
+
+        // Check input for dodging
+        if(Input.GetButtonDown("Fire3"))
+        {
+            return new PlayerDodge(player);
+        }
+
+        // Check input for melee attacking
+        if(Input.GetButtonDown("Fire1"))
+        {
+            // meleeAttack.gameObject.SetActive(true);
+            return new PlayerMelee(player);
+        }
+
+        // Check input for shooting with a projectile
+        if(Input.GetButtonDown("Fire2"))
+        {
+            return new PlayerShoot(player);
+        }
+
+        // If jumps get refreshed, i.e. landing on a platform
+        if(player.maxSpeed == player.groundSpeed &&
+           player.numJumps == playerControl.MAX_JUMPS)
+        {
+            return new PlayerIdle(player);
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        //                                                                 //
+        //  This Chunk of code is also in PlayerWalk                       //
+        //                                                                 //
+        /////////////////////////////////////////////////////////////////////
+        horizontalMovement = Input.GetAxis("Horizontal");
+        if(Mathf.Abs(horizontalMovement) > 0)
+        {
+            player.isFacingRight = (horizontalMovement < 0) ? false : true;
+        }
         return null;
     }
 }
