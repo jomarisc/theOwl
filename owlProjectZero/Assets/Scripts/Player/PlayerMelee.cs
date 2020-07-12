72d0ceedb6e75a1cd8e62433a50097405bde1fb8
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-12 03:51:31 -0700

    Implemented PlayerAttack state

--- owlProjectZero/Assets/Scripts/Player/PlayerMelee.cs
+++ owlProjectZero/Assets/Scripts/Player/PlayerMelee.cs
@@ -5,28 +5,50 @@ using UnityEngine;
 public class PlayerMelee : IState
 {
     private readonly playerControl player;
+    private GameObject meleeAttack;
 
     public PlayerMelee(playerControl p)
     {
         player = p;
+        meleeAttack = p.meleeAttack.gameObject;
     }
     public void Enter()
     {
+        // use melee attack animation here
 
+        meleeAttack.SetActive(true);
     }
 
     public void Exit()
     {
-
+        // For assurance that the hitbox gets deactivated
+        if(meleeAttack.activeInHierarchy)
+        {
+            meleeAttack.SetActive(false);
+        }
     }
 
     public void FixedUpdate()
     {
-
+        player.Attack();
     }
 
     public IState Update()
     {
+        // When the hitbox deactivates
+        if(!meleeAttack.activeInHierarchy)
+        {
+            if(player.maxSpeed == player.airSpeed &&
+               player.numJumps < playerControl.MAX_JUMPS)
+            {
+                return new PlayerJump(player);
+            }
+            else
+            {
+                return new PlayerIdle(player);
+            }
+        }
+        // No cancels yet
         return null;
     }
 }
