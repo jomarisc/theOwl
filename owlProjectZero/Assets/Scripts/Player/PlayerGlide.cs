82c6a0b78a8fea9eae857985c81c81d4bb252295
Charles Advincula <chadvincula7636428@gmail.com>
2020-07-13 20:23:43 -0700

    Glide state can now transition into idle state

--- owlProjectZero/Assets/Scripts/Player/PlayerGlide.cs
+++ owlProjectZero/Assets/Scripts/Player/PlayerGlide.cs
@@ -57,6 +57,19 @@ public class PlayerGlide : IState
         {
             return new PlayerShoot(player);
         }
+
+        // If jumps get refreshed, i.e. landing on a platform
+        if(player.maxSpeed == player.groundSpeed &&
+           player.numJumps == playerControl.MAX_JUMPS)
+        {
+            return new PlayerIdle(player);
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        //                                                                 //
+        //  This Chunk of code is also in PlayerWalk                       //
+        //                                                                 //
+        /////////////////////////////////////////////////////////////////////
         horizontalMovement = Input.GetAxis("Horizontal");
         if(Mathf.Abs(horizontalMovement) > 0)
         {
