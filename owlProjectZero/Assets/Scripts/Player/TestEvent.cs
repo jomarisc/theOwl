96b0e611526e2bc556cb6ddc983dfc63f48e297a
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2020-07-25 18:50:57 -0700

    Wrote TestEvent.cs. Created TestEventSubscriber.cs.

--- owlProjectZero/Assets/Scripts/Player/TestEvent.cs
+++ owlProjectZero/Assets/Scripts/Player/TestEvent.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TestEvent : MonoBehaviour
 {
+    public event EventHandler OnSpacePressed;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        OnSpacePressed += Testing_OnSpacePressed;
+    }
+
+    // In order to subscribe to event, we need a function that will
+    // receive that event. Function signature needs to be same as event.
+    private void Testing_OnSpacePressed(object sender, EventArgs e)
+    {
+
+        Debug.Log("Space pressed!");
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Space pressed!
+            if (OnSpacePressed != null) OnSpacePressed(this, EventArgs.Empty);
+
+        }
     }
 }
