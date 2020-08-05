0bedd82b2ca427353afb10a557bc887f0df2e7f7
Charles Advincula <chadvincula7636428@gmail.com>
2020-08-05 16:45:18 -0700

    Grounded Enemy can specify its max jumps, dodges, and duration in constructor now

---
+++ owlProjectZero/Assets/Scripts/Enemies/GroundedEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedEnemy : Enemy
+{
+    // Initializing maxJumps, maxDodges, dodgeDuration
+    public GroundedEnemy() : base(0, 1, 5f)
+    {}
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    new private void Update()
+    {
+        base.Update();
+    }
+}
