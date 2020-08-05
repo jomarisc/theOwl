070f569315d649fc08859b54bd668394ce26f01b
jomarisc <jomarisc@ucsc.edu>
2020-08-04 18:58:58 -0700

    MenusImplemented
    Finished the base for the main menu and pause menu.

---
+++ owlProjectZero/Assets/Scripts/Flash.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Flash : MonoBehaviour
+{
+    public Image img;
+    public GameObject theButton;
+
+    private float timer;
+    private Vector4 colortest;
+    
+   
+    private void Start()
+    {
+        //img = GetComponent<Image>();
+        colortest = img.color;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 0.5)
+        {
+            colortest = new Vector4(0f, 0f, 0f, 0f);
+            img.color = colortest;
+        }
+        if (timer >= 1)
+        {
+            colortest = new Vector4(255f, 255f, 255f, 255f);
+            img.color = colortest;
+            timer = 0;
+        }
+    }
+
+    public void SelectButton()
+    {
+        EventSystem.current.SetSelectedGameObject(theButton);
+    }
+}
---
+++ owlProjectZero/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausemenu;
+    public GameObject resumebutton;
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausemenu.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused) UnpauseGame();
+            else PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausemenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        EventSystem.current.SetSelectedGameObject(resumebutton);
+    }
+
+    public void UnpauseGame()
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
