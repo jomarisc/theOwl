6a59707ab851e5604123998922f7949d7896af8b
Charles Advincula <chadvincula7636428@gmail.com>
2020-08-11 14:00:42 -0700

    Added more enemies to scene and text to diaplay defeated enemies

--- owlProjectZero/Assets/Scripts/GameSettings.cs
+++ owlProjectZero/Assets/Scripts/GameSettings.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSettings : MonoBehaviour
 {
+    public Text defeatedEnemies;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,17 @@ public class GameSettings : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Enemy.numDefeatedEnemies >= Enemy.totalEnemies)
+        {
+            Win();
+        }
+    }
+
+    private void Win()
+    {
+        defeatedEnemies.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        defeatedEnemies.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+        defeatedEnemies.rectTransform.anchoredPosition = new Vector2(0f, 0f);
+        defeatedEnemies.text = "YOU WIN!";
     }
 }
