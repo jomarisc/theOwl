5f2040e1af5b3574e3343037b5678441439f2a72
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2020-08-18 12:50:10 -0700

    Created class for player skills.

---
+++ owlProjectZero/Assets/Scripts/Player/PlayerSkills.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkills
+{
+    public enum SkillType
+    {
+        Shield,
+    }
+
+    public void UnlockSkill(SkillType skillType)
+    {
+        unlockedSkillTypeList = new List<SkillType>();
+    }
+
+    public void UnlockSkill(SkillType skillType)
+    {
+        unlockedSkillTypeList.Add(skillType);
+    }
+
+    public bool IsSkillUnlocked(SkillType skillType)
+    {
+        return unlockedSkillTypeList.Contains(skillType);
+    }
+}
