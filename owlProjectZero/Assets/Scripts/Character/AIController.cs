620f18ca7df7c67adc10e3023380bcc926189efe
Charles Advincula <chadvincula7636428@gmail.com>
2020-08-30 19:16:35 -0700

    Only enemy behavior gets turned off when offscreen

---
+++ owlProjectZero/Assets/Scripts/Character/AIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    private Character myBehavior;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBehavior = gameObject.GetComponent<Character>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnBecameVisible()
+    {
+        Debug.Log("Visible");
+        myBehavior.enabled = true;
+    }
+
+    void OnBecameInvisible()
+    {
+        Debug.Log("Invisible");
+        myBehavior.enabled = false;
+    }
+}
