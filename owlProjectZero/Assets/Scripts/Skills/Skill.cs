976e26bcbedd2cce15f3398527242cd063980d5f
Charles Advincula <chadvincula7636428@gmail.com>
2020-09-01 16:27:10 -0700

    Created generic Skill class

---
+++ owlProjectZero/Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Skill : MonoBehaviour, ISkill
+{
+    // private fields
+
+    // protected fields
+    [SerializeField] protected float stamanaCost;
+    [SerializeField] protected float cooldown;
+    [SerializeField] protected float passiveMultiplier;
+    [SerializeField] protected float currencyCost;
+
+    // public fields
+    public enum SkillType
+    {
+        None, Offensive, Defensive, Utility
+    }
+
+    public int requiredLevel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UseSkill()
+    {
+
+    }
+}
