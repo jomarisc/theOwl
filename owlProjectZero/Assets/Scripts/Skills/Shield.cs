b5f624f5626bef088862dd7f0444e6544618c6d0
Charles Advincula <chadvincula7636428@gmail.com>
2020-09-01 17:43:26 -0700

    Created first pass at Shield implementation

--- owlProjectZero/Assets/Scripts/Skills/Shield.cs
+++ owlProjectZero/Assets/Scripts/Skills/Shield.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Collider))]
 public class Shield : Skill
 {
-    // Start is called before the first frame update
-    void Start()
+    Shield()
     {
-        
+        type = SkillType.Defensive;
+    }
+    // Start is called before the first frame update
+    new private void Start()
+    {
+
     }
 
     // Update is called once per frame
-    void Update()
+    new private void Update()
     {
         
+    }
+    
+    public override void UseSkill()
+    {
+        if(cooldown >= maxCooldown)
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+            gameObject.GetComponent<Collider>().enabled = true;
+            isActive = true;
+        }
+        else
+        {
+            isActive = false;
+            Debug.Log("Shield is still in cooldown!");
+        }
+    }
+
+    public override void DeactivateSkill()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+        gameObject.GetComponent<Collider>().enabled = false;
     }
 }
