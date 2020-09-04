0f6d753831216903dd8fea438958310828629548
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2020-09-04 03:23:04 -0700

    SkillTreeDataStructure node creation
    Working on defining a node for the skill tree data structure. Currently it will be a node that has values of type LinkedList<LinkedList<int>>. Goal of this class is to link together these Nodes of type LinkedList<LinkedList<int>>.

---
+++ owlProjectZero/Assets/Scripts/Skills/SkillTreeDataStructure.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeDataStructure
+{
+    // Re-evaluate if a struct is needed or not
+    struct Node
+    {
+        LinkedList<LinkedList<int>> dataLinkedList;
+        string dataName;
+
+        Node* left, mid, right;
+    };
+
+    Node* newNode(LinkedList<LinkedList<int>> data)
+    {
+        // Attempting to create a node which its value
+        // is a linked list of int-typed linked lists
+        LinkedListNode<LinkedList<LinkedList<int>>>* node = new 
+            LinkedListNode<LinkedList<LinkedList<int>>>(new LinkedList<LinkedList<int>>());
+        string dataName = "Category"; 
+        node->Value = data;
+        node->Next = null;
+        node->Prevous = null;
+
+        return node;
+
+    }
+
+}
