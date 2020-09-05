7a504c694c0f4537f98ef484d24f269070d3d65b
Charles Advincula <chadvincula7636428@gmail.com>
2020-09-04 23:56:00 -0700

    Active skill icon takes the correct icon at the start of booting up a scene

---
+++ owlProjectZero/Assets/Scripts/Skills/ActiveSkillUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// This class is in charge of updating the active skill UI when necessary
+[RequireComponent(typeof(Image))]
+public class ActiveSkillUI : MonoBehaviour
+{
+    [SerializeField] private PlayerSkills equippedSkills;
+    private Image currentSkillIcon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSkillIcon = gameObject.GetComponent<Image>();
+        currentSkillIcon.sprite = equippedSkills.currentSkill.GetIcon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
