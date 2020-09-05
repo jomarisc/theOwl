a197893833bbf50464a788beae897aecae5bd758
Charles Advincula <chadvincula7636428@gmail.com>
2020-09-04 23:32:41 -0700

    Created Empty skill class for test purposes

---
+++ owlProjectZero/Assets/Scripts/Skills/EmptySkill.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptySkill : Skill
+{
+    EmptySkill()
+    {
+        type = SkillType.None;
+    }
+    // Start is called before the first frame update
+    new private void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    new private void Update()
+    {
+        base.Update();
+    }
+
+    public override void UseSkill()
+    {
+
+    }
+
+    public override void DeactivateSkill()
+    {
+        
+    }
+}
