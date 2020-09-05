44cfd99ede8fcfcbbd14a40a322328856f566a65
Charles Advincula <chadvincula7636428@gmail.com>
2020-09-04 23:54:29 -0700

    Made EmptySkill.cs more readable

--- owlProjectZero/Assets/Scripts/Skills/EmptySkill.cs
+++ owlProjectZero/Assets/Scripts/Skills/EmptySkill.cs
@@ -8,25 +8,6 @@ public class EmptySkill : Skill
     {
         type = SkillType.None;
     }
-    // Start is called before the first frame update
-    new private void Start()
-    {
-        base.Start();
-    }
-
-    // Update is called once per frame
-    new private void Update()
-    {
-        base.Update();
-    }
-
-    public override void UseSkill()
-    {
-
-    }
-
-    public override void DeactivateSkill()
-    {
-        
-    }
+    public override void UseSkill() {}
+    public override void DeactivateSkill() {}
 }
