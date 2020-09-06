b3f02b99ea1df585896d815e1c86093a789fb80b
Charles Advincula <chadvincula7636428@gmail.com>
2020-09-06 15:42:08 -0700

    Created Skill Wheel UI script

---
+++ owlProjectZero/Assets/Scripts/Skills/SkillWheelUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// This class is in charge of updating the skill wheel UI when necessary
+[RequireComponent(typeof(Image))]
+public class SkillWheelUI : MonoBehaviour
+{
+    [SerializeField] private PlayerSkills equippedSkills;
+    [SerializeField] private int slotNumber;
+    private Image myIcon;
+
+    void Awake()
+    {
+        myIcon = gameObject.GetComponent<Image>();
+    }
+    
+    void OnEnable()
+    {
+        myIcon.sprite = equippedSkills.unlockedSkillTypeList[slotNumber - 1].GetIcon();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
