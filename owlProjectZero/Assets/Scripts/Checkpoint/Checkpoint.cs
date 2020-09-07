3b3e2990ee709e8fc35b9a00a24997a7623d4d96
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2020-09-06 18:35:11 -0700

    Checkpoint Test Scene / Checkpoint prefab
    Created a test scene for checkpoint and created a Checkpoint prefab.

--- owlProjectZero/Assets/Scripts/Checkpoint/Checkpoint.cs
+++ owlProjectZero/Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public enum state
+    {
+      Inactive, Active, Used, Locked  
+    };
+
+    public state status;
+
+    public Sprite[] sprites;
+
     void Update()
     {
-        
+        ChangeColor();
+    }
+    
+    // Update is called once per frame
+    void ChangeColor()
+    {
+        if(status == state.Inactive)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+        else if (status == state.Active)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[1];
+        }
+        else if (status == state.Used)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[2];
+        }
+        else if (status == state.Locked)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[3];
+        }
     }
 }
