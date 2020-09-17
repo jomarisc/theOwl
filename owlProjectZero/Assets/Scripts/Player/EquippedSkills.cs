7bd4784f3c936ef646548ba9e2c143dec6a84229
Charles Advincula <chadvincula7636428@gmail.com>
2020-09-16 19:11:27 -0700

    Created Equipped Skills component

---
+++ owlProjectZero/Assets/Scripts/Player/EquippedSkills.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedSkills : MonoBehaviour
+{
+    
+    //private readonly playerControl player;
+
+    public Skill currentSkill;
+    public Skill[] skills = new Skill[4];
+}
