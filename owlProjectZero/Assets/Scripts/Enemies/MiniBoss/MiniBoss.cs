9d6f7be6e2547d094b26d6f83d34cf083a9590fd
Charles Advincula <chadvincula7636428@gmail.com>
2020-09-24 21:47:45 -0700

    Created Mini Boss Component

---
+++ owlProjectZero/Assets/Scripts/Enemies/MiniBoss/MiniBoss.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBoss : HeavyEnemy
+{
+    void OnEnable()
+    {
+        if(myState == null)
+        {
+            defaultState = new MiniBossIdle(this);
+            myState = defaultState;
+            Debug.Log(myState);
+        }
+    }
+}
