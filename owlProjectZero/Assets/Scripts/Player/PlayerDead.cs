d43cd2341198be801f1069a1807a06dd46756d94
Charles Advincula <chadvincula7636428@gmail.com>
2020-10-17 00:01:44 -0700

    Finished the death animation

--- owlProjectZero/Assets/Scripts/Player/PlayerDead.cs
+++ owlProjectZero/Assets/Scripts/Player/PlayerDead.cs
@@ -6,14 +6,24 @@ using UnityEngine.SceneManagement;
 public class PlayerDead : IState
 {
     private readonly playerControl player;
+    private Animator animator;
+    private Rigidbody playerBody;
+    private SpriteRenderer spriteRenderer;
+    private string myAnimationState;
     
     public PlayerDead(playerControl p)
     {
         player = p;
+        animator = p.animator;
+        playerBody = p.GetComponent<Rigidbody>();
+        spriteRenderer = p.GetComponent<SpriteRenderer>();
     }
 
     public void Enter()
     {
+        myAnimationState = "PlayerHurt";
+        animator.Play(myAnimationState);
+
         player.GetComponent<SpriteRenderer>().color = Color.red;
         player.dodgeAbility.PerformDodge();
         player.data.deadDuration = player.CONSTANTS.DEAD_DURATION;
@@ -37,12 +47,26 @@ public class PlayerDead : IState
 
     public IState Update()
     {
+        if(playerBody.velocity.x < 0)
+            spriteRenderer.flipX = false;
+        else if(playerBody.velocity.x > 0)
+            spriteRenderer.flipX = true;
+
+        if(myAnimationState.Equals("PlayerHurt") &&
+           player.data.maxSpeed == player.data.groundSpeed &&
+           playerBody.velocity.y == 0f)
+        {
+            myAnimationState = "PlayerDead";
+            animator.Play(myAnimationState);
+        }
+
         if (player.data.deadDuration >= 0f)
         {
             player.data.deadDuration -= Time.deltaTime; // 3.0 - The completion time in seconds since the last frame
             return null;
         }
-
+        if(player.data.maxSpeed == player.data.airSpeed)
+            return new PlayerMove(player, true);
         return new PlayerIdle(player);
     }
 
