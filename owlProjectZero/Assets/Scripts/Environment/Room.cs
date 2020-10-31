609c91940f3144adf7e60715b9d2f15e50d0ad50
Charles Advincula <chadvincula7636428@gmail.com>
2020-10-31 00:55:53 -0700

    Created Room class

---
+++ owlProjectZero/Assets/Scripts/Environment/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Room : MonoBehaviour
+{
+    private Collider box;
+    [SerializeField] private Collider leftWall;
+    [SerializeField] private Collider rightWall;
+    [SerializeField] private string unlockCondition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.TryGetComponent(out playerControl player))
+        {
+            leftWall.enabled = true;
+            rightWall.enabled = true;
+        }
+    }
+}
