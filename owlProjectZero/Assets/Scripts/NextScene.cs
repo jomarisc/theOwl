e63956b36bc79206a3ed0c8c9624c34ab9e1bf98
Charles Advincula <chadvincula7636428@gmail.com>
2020-12-05 12:54:03 -0800

    Added Scene Triggers that loads the next scene

--- owlProjectZero/Assets/Scripts/NextScene.cs
+++ owlProjectZero/Assets/Scripts/NextScene.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Collider))]
 public class NextScene : MonoBehaviour
 {
+    public int sceneIndex;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,10 @@ public class NextScene : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        SceneManager.LoadSceneAsync(sceneIndex);
     }
 }
