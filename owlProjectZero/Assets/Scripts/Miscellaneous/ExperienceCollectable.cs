7b79f956616cf2d7a2c5b1c44807468f91ed458d
Charles Advincula <chadvincula7636428@gmail.com>
2020-12-22 18:15:49 -0800

    Split up the logic for different types of collectables (Experience and Skill Currency)

---
+++ owlProjectZero/Assets/Scripts/Miscellaneous/ExperienceCollectable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperienceCollectable : Collectable
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.GetComponent<playerControl>() != null)
+        {
+            //gameObject.SetActive(false);
+            Destroy(this.gameObject);
+
+
+            // Refers to the levelSystem of the collider passed into this function.
+            // Then uses AddExperience of this levelSystem.
+            col.gameObject.GetComponent<playerControl>().levelSystem.AddExperience(50);
+            
+        }
+    }
+}
