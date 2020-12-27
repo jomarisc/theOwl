36d679ad02307d454272cd2c47e92ce9033d08a9
Charles Advincula <chadvincula7636428@gmail.com>
2020-12-26 17:12:18 -0800

    PersistentAudioSource script works now, but only one instance of this class can remain in a given scene

--- owlProjectZero/Assets/Scripts/Audio/PersistentAudioSource.cs
+++ owlProjectZero/Assets/Scripts/Audio/PersistentAudioSource.cs
@@ -2,8 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class PersistentAudioSource : MonoBehaviour
 {
+    private static PersistentAudioSource instance;
+    public static PersistentAudioSource Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
