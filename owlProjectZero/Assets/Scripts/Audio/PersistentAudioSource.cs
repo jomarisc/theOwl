1557d0dc3622d386c239ba97912fa866780fadd2
Charles Advincula <chadvincula7636428@gmail.com>
2020-12-26 18:03:47 -0800

    BGM audio clip can be overwritten if the new audio clip is different from the last one

--- owlProjectZero/Assets/Scripts/Audio/PersistentAudioSource.cs
+++ owlProjectZero/Assets/Scripts/Audio/PersistentAudioSource.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class PersistentAudioSource : MonoBehaviour
 {
+    // Singleton instance variables
     private static PersistentAudioSource instance;
     public static PersistentAudioSource Instance
     {
@@ -13,23 +14,26 @@ public class PersistentAudioSource : MonoBehaviour
 
     void Awake()
     {
+        AudioSource myAudio = GetComponent<AudioSource>();
         if(instance != null && instance != this)
         {
-            Destroy(this.gameObject);
-            return;
+            // If there's a persistent audio source with a different audio clip,
+            // destroy the current instance of the persistent audio source
+            if(myAudio.clip != instance.GetComponent<AudioSource>().clip)
+            {
+                Destroy(instance.gameObject);
+            }
+            // Else, destroy the second instance of a persistent audio source
+            // that plays the same audio clip
+            else
+            {
+                Destroy(this.gameObject);
+                return;
+            }
         }
+
+        // Overwrite the current instance of the persistent audio source
         instance = this;
         DontDestroyOnLoad(this.gameObject);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
