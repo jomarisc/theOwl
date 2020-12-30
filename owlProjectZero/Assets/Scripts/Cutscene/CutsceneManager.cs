b244855265f8b17d537a34e0f327b8381edb6ec3
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2020-12-30 14:05:04 -0800

    Created CutsceneManager in Area1 Room4
    Created CutsceneManager game object and CutsceneCollision game object.

---
+++ owlProjectZero/Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneManager : MonoBehaviour
+{
+    private CutsceneCollision localCutsceneCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Continue from here: 12/30/2020
+        localCutsceneCollider = GetComponent<CutsceneCollision>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
