de94f111215603ef63cd6d7261e962b5451e702f
Charles Advincula <chadvincula7636428@gmail.com>
2021-01-09 11:37:07 -0800

    First iteration of how cutscene editing might look like in the inspector

--- owlProjectZero/Assets/Scripts/Cutscene/CutsceneManager.cs
+++ owlProjectZero/Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -3,12 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct CutsceneAction
+{
+    public Character actor;
+    public AnimationClip action;
+}
+
+[System.Serializable]
+public struct CutsceneSequence
+{
+    public CutsceneAction[] directions;
+}
+
 public class CutsceneManager : MonoBehaviour
 {
     private CutsceneCollider localCutsceneCollider;
     private Character[] characters;
     private Canvas gameplayCanvas;
     private Canvas cutsceneCanvas;
+    public CutsceneSequence[] stageDirections; // The cutscene manager will iterate through this
+                                               // array as the player progresses through the text
 
     void Awake()
     {
