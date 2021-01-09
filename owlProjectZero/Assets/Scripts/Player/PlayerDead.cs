f3e462602137187521523fbd9240df196dc2b626
Charles Advincula <chadvincula7636428@gmail.com>
2021-01-09 12:47:54 -0800

    Renamed player death state to match the name of the animation clip

--- owlProjectZero/Assets/Scripts/Player/PlayerDead.cs
+++ owlProjectZero/Assets/Scripts/Player/PlayerDead.cs
@@ -57,7 +57,7 @@ public class PlayerDead : IState
            player.data.maxSpeed == player.data.groundSpeed &&
            playerBody.velocity.y == 0f)
         {
-            myAnimationState = "PlayerDead";
+            myAnimationState = "PlayerDeath";
             animator.Play(myAnimationState);
         }
 
