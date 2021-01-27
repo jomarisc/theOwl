76ce5f68701528b40176668b18eee889f87f878e
Charles Advincula <chadvincula7636428@gmail.com>
2021-01-27 15:59:15 -0800

    Scene triggers now play scene transitions
    The object references must be manually attached, however

--- owlProjectZero/Assets/Scripts/NextScene.cs
+++ owlProjectZero/Assets/Scripts/NextScene.cs
@@ -2,25 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 [RequireComponent(typeof(Collider))]
 public class NextScene : MonoBehaviour
 {
+    private playerControl player;
+    private PlayableDirector myDirector;
+    [Tooltip("Timeline asset for entering a new scene")]
+    [SerializeField] private PlayableAsset startTransition;
+    [Tooltip("Timeline asset for moving on to a different scene")]
+    [SerializeField] private PlayableAsset endTransition;
     public int sceneIndex;
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        
+        player = GameObject.Find("player").GetComponent<playerControl>();
+        player.enabled = false;
+
+        myDirector = GetComponent<PlayableDirector>();
+        myDirector.playableAsset = startTransition;
+        myDirector.Play();
+        myDirector.stopped += ReenablePlayer;
+
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
-        
+        myDirector.stopped -= LoadNextScene;
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        SceneManager.LoadSceneAsync(sceneIndex);
+        myDirector.playableAsset = endTransition;
+        myDirector.Play();
+        player.enabled = false;
+        myDirector.stopped += LoadNextScene;
+    }
+
+    void LoadNextScene(PlayableDirector director)
+    {
+        if(director == myDirector)
+            SceneManager.LoadSceneAsync(sceneIndex);
+        Destroy(this.gameObject);
+    }
+
+    void ReenablePlayer(PlayableDirector director)
+    {
+        if(director == myDirector)
+        {
+            player.enabled = true;
+            myDirector.stopped -= ReenablePlayer;
+        }
     }
 }
