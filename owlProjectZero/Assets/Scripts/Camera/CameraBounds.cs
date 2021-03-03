c3aa7509986601fd4a66231cb959e0f2023a3775
Charles Advincula <chadvincula7636428@gmail.com>
2021-03-02 21:04:06 -0800

    Removed redundant code in CameraBounds script

--- owlProjectZero/Assets/Scripts/Camera/CameraBounds.cs
+++ owlProjectZero/Assets/Scripts/Camera/CameraBounds.cs
@@ -7,36 +7,12 @@ using Cinemachine;
 public class CameraBounds : MonoBehaviour
 {
     private BoxCollider myBounds;
-    [SerializeField] private CinemachineVirtualCamera myVirtualCamera;
     [SerializeField] private CinemachineMixingCamera myMixingCamera;
-    public static CinemachineVirtualCamera[] virtualCameras;
 
     void Awake()
     {
-        // Get all virtual cameras within the scene
-        virtualCameras = GameObject.FindObjectsOfType<CinemachineVirtualCamera>();
         myBounds = GetComponent<BoxCollider>();
     }
-
-    // void OnTriggerEnter(Collider col)
-    // {
-    //     // Make sure that this gameobject's physics layer only checks for collisions with the player
-    //     // foreach (CinemachineVirtualCamera camera in virtualCameras)
-    //     // {
-    //     //     if(camera == myVirtualCamera)
-    //     //         myVirtualCamera.Priority = 2;
-    //     //     else
-    //     //         camera.Priority = 1;
-    //     // }
-    //     // myVirtualCamera.Follow = col.transform;
-    //     // myVirtualCamera.LookAt = col.transform;
-    //     // myVirtualCamera.Priority = 1;
-
-    //     // Start following and looking at the player with the top camera
-    //     int topCamera = 0;
-    //     myMixingCamera.ChildCameras[topCamera].Follow = col.transform;
-    //     myMixingCamera.ChildCameras[topCamera].LookAt = col.transform;
-    // }
 
     void OnTriggerStay(Collider col)
     {
@@ -49,21 +25,6 @@ public class CameraBounds : MonoBehaviour
         float bottomCameraPriority = (transform.lossyScale.y - (col.transform.position.y - bottomOfBounds)) + 1f;
         myMixingCamera.ChildCameras[bottomCamera].Priority = (int)bottomCameraPriority;
         myMixingCamera.SetWeight(bottomCamera, bottomCameraPriority);
-        Debug.Log("Bottom of Bounds: " + bottomOfBounds + "\nPlayer PosY: " + col.transform.position.y);
-        // Vector3 localPlayerPos = col.transform.position - transform.position;
-        // Debug.Log("Local Y: " + localPlayerPos.y);
     }
-
-    // void OnTriggerExit(Collider col)
-    // {
-    //     // myVirtualCamera.Follow = null;
-    //     // myVirtualCamera.LookAt = null;
-    //     // myVirtualCamera.Priority = 0;
-        
-    //     // Stop Following and Looking with the top camera
-    //     int topCamera = 0;
-    //     myMixingCamera.ChildCameras[topCamera].Follow = null;
-    //     myMixingCamera.ChildCameras[topCamera].LookAt = null;
-    // }
 }
 
