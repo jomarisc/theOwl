5b98d0760cb1d1cd5ed7b4d0aed69ed99083ec82
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-03-13 15:09:44 -0800

    changed melee and ground enemy stats
    - melee kb and kb angle
    - grounded enemy mass and drag
    - edited attack script to impulse

--- owlProjectZero/Assets/Scripts/Character/Attack.cs
+++ owlProjectZero/Assets/Scripts/Character/Attack.cs
@@ -206,7 +206,7 @@ public abstract class Attack : MonoBehaviour
                     float adjustedKBAngle = (isFacingRight) ? hitboxes[0].knockbackAngle : 180 - hitboxes[0].knockbackAngle;
                     Vector3 knockback = KnockbackForce(hitboxes[0].knockback, adjustedKBAngle);
                     col.attachedRigidbody.velocity = Vector3.zero;
-                    col.attachedRigidbody.AddForce(knockback, ForceMode.VelocityChange);
+                    col.attachedRigidbody.AddForce(knockback, ForceMode.Impulse);
                     
                     // Go to damaged state
                     col.gameObject.GetComponent<Character>().GoToDamagedState(hitboxes[0].damage, hitboxes[0].knockback);
