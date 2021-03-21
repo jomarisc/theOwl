99ab8b69aedda1d97fe05a6953b8ace0a051ab3a
Charles Advincula <chadvincula7636428@gmail.com>
2021-03-20 22:40:16 -0700

    Finished setting up aerial enemy's default idle state

---
+++ owlProjectZero/Assets/Scripts/Enemies/AerialEnemy/AEnemyIdle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AEnemyIdle : IState
+{
+    private readonly AerialEnemy character;
+    private const float IDLE_TIME = 3f;
+    private float waitTime;
+    private bool playerFound;
+
+    public AEnemyIdle(AerialEnemy myself)
+    {
+        character = myself;
+    }
+
+    public void Enter()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+
+    }
+
+    public IState Update()
+    {
+        return null;
+    }
+}
---
+++ owlProjectZero/Assets/Scripts/Enemies/AerialEnemy/AerialEnemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AerialEnemy : Enemy
+{
+    // private fields
+    private SpriteRenderer sRenderer;
+
+    // protected fields
+
+    // public fields
+
+    // Initializing maxJumps, maxDodges, dodgeDuration
+    public AerialEnemy()
+    {}
+
+    void OnEnable()
+    {
+        if(myState == null)
+        {
+            defaultState = new AEnemyIdle(this);
+            myState = defaultState;
+        }
+    }
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody>();
+        sRenderer = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    new void Update()
+    {
+        base.Update();
+        sRenderer.flipX = data.isFacingRight;
+    }
+}
