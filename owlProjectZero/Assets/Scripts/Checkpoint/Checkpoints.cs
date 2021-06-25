c2a22964b0f571b2492f94964a34ef79d7a4946d
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-06-25 04:36:51 -0700

    got a basic checkpoint system working

---
+++ owlProjectZero/Assets/Scripts/Checkpoint/Checkpoints.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Checkpoints : MonoBehaviour
+{
+
+    public string currScene;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        currScene = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("CHECKPOINT SAVED");
+
+        if(other.gameObject.tag == player.tag)
+        {
+            CheckpointsHandler.checkpointScene = currScene;
+            CheckpointsHandler.playerPosition[0] = this.transform.position.x;
+            CheckpointsHandler.playerPosition[1] = this.transform.position.y;
+            CheckpointsHandler.playerPosition[2] = 0.0f;
+        }
+    }
+
+    // void OnTriggerExit(Collider other)
+    // {
+    //     if(other.gameObject.tag == player.tag)
+    //     {
+    //         Debug.Log(CheckpointsHandler.checkpointScene);
+
+    //         for(int i = 0; i < CheckpointsHandler.playerPosition.Length; i++)
+    //         {
+    //             Debug.Log(CheckpointsHandler.playerPosition[i]);
+    //         }
+    //     }
+    // }
+}
---
+++ owlProjectZero/Assets/Scripts/Checkpoint/ReturnToCheckpoint.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToCheckpoint : MonoBehaviour
+{
+
+    public GameObject player;
+    public playerControl playerState;
+
+    void Awake()
+    {
+        player = this.gameObject;
+        playerState = player.GetComponent<playerControl>();
+
+        if(CheckpointsHandler.isDead == true)
+        {
+            player.transform.position = new Vector3(CheckpointsHandler.playerPosition[0], CheckpointsHandler.playerPosition[1], CheckpointsHandler.playerPosition[2]);
+            // playerState.healthbar.ResetHealth();
+            // playerState.healthbar.Redraw();
+            playerState.isAlive = true;
+            CheckpointsHandler.isDead = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerState.isAlive == false)
+        {
+
+            CheckpointsHandler.isDead = true;
+
+            if(playerState.data.deadDuration <= 1f)
+            {
+                SceneManager.LoadScene(CheckpointsHandler.checkpointScene);
+            }
+        }
+    }
+}
