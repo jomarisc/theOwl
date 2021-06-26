0cf7baec0e5b32dbce785e0aea8167e7c452b93c
jomarisc <jomarisc@ucsc.edu>
2021-06-26 14:19:27 -0700

    New Menu
    Main Menu

---
+++ owlProjectZero/Assets/Scripts/UI/StopFlashing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class StopFlashing : MonoBehaviour
+{
+    public Animation[] menuButtons;
+
+    public void FlashPlay()
+    {
+        menuButtons[0].Stop();
+        menuButtons[0].Play();
+    }
+
+    public void FlashOptions()
+    {
+        menuButtons[1].Stop();
+        menuButtons[1].Play();
+    }
+
+    public void FlashQuit()
+    {
+        menuButtons[2].Stop();
+        menuButtons[2].Play();
+    }
+}
