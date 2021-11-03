5dc66adf7826ff6a6357cbcd744d0ba9cac77f43
Charles Advincula <chadvincula7636428@gmail.com>
2021-11-02 23:56:36 -0700

    Returning to the Alley from the Elevator locks Area 1 from the player
    But for some reason I cannot set the Alley as the checkpoint scene

---
+++ owlProjectZero/Assets/Scripts/Checkpoint/LockContent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StoryFlags {hasReachedTheElevator, playerHasUnlockedSuit}
+public class LockContent : MonoBehaviour
+{
+    [SerializeField] private StoryFlags flag;
+    [SerializeField] private GameObject[] contentToLock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LockGameplay(flag);
+    }
+
+    private void LockGameplay(StoryFlags myFlag)
+    {
+        switch(myFlag)
+        {
+            case StoryFlags.hasReachedTheElevator:
+                if(GlobalVars.hasReachedTheElevator)
+                {
+                    // Set default scene to Alley scene
+                    CheckpointsHandler.checkpointScene = 2;
+                    // Lock certain content
+                    foreach (GameObject content in contentToLock)
+                        content.SetActive(false);
+                }
+                break;
+            case StoryFlags.playerHasUnlockedSuit:
+                throw new Exception("Not yet sure what content to lock/unlock if the player has/hasn't unlocked the suit");
+            default:
+                Debug.LogError("What are you even using this script for? lol");
+                break;
+        }
+    }
+}
