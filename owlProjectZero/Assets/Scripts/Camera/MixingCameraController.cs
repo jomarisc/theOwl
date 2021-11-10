3ac13efeaa3f660fa1f8f46e3e2954904b621437
Charles Advincula <chadvincula7636428@gmail.com>
2021-11-09 19:19:05 -0800

    Changed how the mixing camera works

--- owlProjectZero/Assets/Scripts/Camera/MixingCameraController.cs
+++ owlProjectZero/Assets/Scripts/Camera/MixingCameraController.cs
@@ -12,18 +12,38 @@ public class MixingCameraController : MonoBehaviour
     void Awake()
     {
         myBounds = GetComponent<BoxCollider>();
-        myMixingCamera = GameObject.Find("MixingCamera").GetComponent<CinemachineMixingCamera>();
+        myMixingCamera = GetComponentInParent<CinemachineMixingCamera>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        myMixingCamera.Priority = 10;
+        CalculateCameraPriority(col.transform.position.y);
     }
 
     void OnTriggerStay(Collider col)
     {
+        CalculateCameraPriority(col.transform.position.y);
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        int topCamera = 0;
+        int bottomCamera = 1;
+        myMixingCamera.ChildCameras[topCamera].Priority = 0;
+        myMixingCamera.ChildCameras[bottomCamera].Priority = 0;
+        myMixingCamera.Priority = 0;
+    }
+
+    void CalculateCameraPriority(float yPosition)
+    {
         int topCamera = 0;
         int bottomCamera = 1;
         float bottomOfBounds = myBounds.transform.position.y - myBounds.size.y * transform.lossyScale.y / 2;
-        float topCameraPriority = (col.transform.position.y - bottomOfBounds) + 1f;
+        float topCameraPriority = (yPosition - bottomOfBounds) + 1f;
         myMixingCamera.ChildCameras[topCamera].Priority = (int)topCameraPriority;
         myMixingCamera.SetWeight(topCamera, topCameraPriority);
-        float bottomCameraPriority = (transform.lossyScale.y - (col.transform.position.y - bottomOfBounds)) + 1f;
+        float bottomCameraPriority = (transform.lossyScale.y - (yPosition - bottomOfBounds)) + 1f;
         myMixingCamera.ChildCameras[bottomCamera].Priority = (int)bottomCameraPriority;
         myMixingCamera.SetWeight(bottomCamera, bottomCameraPriority);
     }
