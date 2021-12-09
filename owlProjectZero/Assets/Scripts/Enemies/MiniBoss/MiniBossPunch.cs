24dfa2260b79afe84917f5031330b1c1d1c9dc32
Charles Advincula <chadvincula7636428@gmail.com>
2021-12-09 00:14:07 -0800

    Implemented lunge attack animation

--- owlProjectZero/Assets/Scripts/Enemies/MiniBoss/MiniBossPunch.cs
+++ owlProjectZero/Assets/Scripts/Enemies/MiniBoss/MiniBossPunch.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class MiniBossPunch : IState
 {
     private readonly MiniBoss character;
+    private Animator animator;
+    private string myAnimationState;
     private Rigidbody characterBody;
     private GameObject meleeAttack;
     private Collider hitbox;
     private float horizontalMovement;
+    private bool playerIsInSight;
+    private bool playerIsInAttackRange;
     
     public MiniBossPunch(MiniBoss myself, float hm)
     {
         character = (MiniBoss)myself;
+        animator = myself.GetComponent<Animator>();
         characterBody = myself.GetComponent<Rigidbody>();
         meleeAttack = myself.basicAttack.gameObject;
         hitbox = meleeAttack.GetComponentInChildren<Collider>();
@@ -22,14 +27,20 @@ public class MiniBossPunch : IState
     public void Enter()
     {
         // Enter grounded enemy walk animation here:
+        animator.SetBool("windingUp", true);
         
         characterBody.velocity = Vector3.zero;
         meleeAttack.SetActive(true);
         character.Attack();
+        playerIsInSight = false;
+        playerIsInAttackRange = false;
     }
 
     public void Exit()
     {
+        if(animator.GetBool("windingUp"))
+            animator.SetBool("windingUp", false);
+
         if(meleeAttack.activeInHierarchy)
         {
             meleeAttack.SetActive(false);
@@ -39,13 +50,29 @@ public class MiniBossPunch : IState
     public void FixedUpdate()
     {
         if(hitbox.enabled)
-            characterBody.AddForce(new Vector3(horizontalMovement * 1.5f, 0f, 0f), ForceMode.VelocityChange);
+            characterBody.AddForce(new Vector3(horizontalMovement * 2f, 0f, 0f), ForceMode.VelocityChange);
     }
     
     public IState Update()
     {
+        if(hitbox.enabled && animator.GetBool("windingUp"))
+        {
+            animator.SetBool("windingUp", false);
+            myAnimationState = "MiniBossRush"; // Temporary
+            animator.Play(myAnimationState);
+        }
+
         if(!meleeAttack.activeInHierarchy)
         {
+            playerIsInSight = character.SeesPlayer();
+
+            if(playerIsInSight)
+            {
+                playerIsInAttackRange = character.PlayerInAttackRange(character.basicAttack.transform.localPosition.x + character.basicAttack.transform.localScale.x * 10); // 2f comes from scaleY from hitbox's transform component
+            }
+            
+            if(playerIsInAttackRange)
+                return new MiniBossPunch(character, horizontalMovement);
             Debug.Log("Returning to idle");
             return new MiniBossIdle(character);
         }
